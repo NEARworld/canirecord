92a5cc6befd51a58612415d76fbb63e053296d52
nearworld <artemismars2@outlook.com>
2024-01-31 16:11:00 +0900

    feat: added login button

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -15,8 +15,11 @@ import {
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "next-auth/react";
+import { Luckiest_Guy } from "next/font/google";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import z from "zod";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 const formSchema: z.ZodSchema = z
   .object({
@@ -49,75 +52,82 @@ export default function Home() {
   };
 
   return (
-    <Card className="w-1/5 grid place-items-center py-8">
-      <Form {...form}>
-        <form
-          style={{ width: 250 }}
-          action={createUser}
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-          <FormField
-            name={"email"}
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel htmlFor={field.name}>email</FormLabel>
-                <FormDescription>please input your email</FormDescription>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />{" "}
-          <FormField
-            name={"username"}
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel htmlFor={field.name}>username</FormLabel>
-                <FormDescription>please input your username</FormDescription>
-                <FormControl>
-                  <Input placeholder="min 2, max 6" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            name={"password"}
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel htmlFor={field.name}>password</FormLabel>
-                <FormDescription>please input your password</FormDescription>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="min 6, max 8"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            name={"confirm"}
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel htmlFor={field.name}>confirm password</FormLabel>
-                <FormControl>
-                  <Input type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button className="float-right">sign up</Button>
-        </form>
-      </Form>
-    </Card>
+    <section className="w-full grid place-items-center">
+      <header className="w-1/5 text-start opacity-50">
+        <Link href="/" className="inline-flex">
+          <ArrowLeft /> <span>back</span>
+        </Link>
+      </header>
+      <Card className="w-1/5 grid place-items-center py-8 mt-2">
+        <Form {...form}>
+          <form
+            style={{ width: 250 }}
+            action={createUser}
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              name={"email"}
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor={field.name}>email</FormLabel>
+                  <FormDescription>please input your email</FormDescription>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />{" "}
+            <FormField
+              name={"username"}
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor={field.name}>username</FormLabel>
+                  <FormDescription>please input your username</FormDescription>
+                  <FormControl>
+                    <Input placeholder="min 2, max 6" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name={"password"}
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor={field.name}>password</FormLabel>
+                  <FormDescription>please input your password</FormDescription>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="min 6, max 8"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name={"confirm"}
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor={field.name}>confirm password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="float-right">sign up</Button>
+          </form>
+        </Form>
+      </Card>
+    </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,14 @@ export default function Home() {
   return (
     <main className="min-h-[100vh] w-full grid place-items-center">
       <section className="w-2/3 h-5/6 flex flex-col justify-center items-center gap-5">
-        <Link href="auth/signup" className="self-end">
-          <Button>signup</Button>
-        </Link>
+        <header className="self-end flex gap-2">
+          <Link href="auth/signup">
+            <Button>signup</Button>
+          </Link>
+          <Link href="auth/signin">
+            <Button>login</Button>
+          </Link>
+        </header>
         <Card className="w-full h-[70vh]"></Card>
       </section>
     </main>
