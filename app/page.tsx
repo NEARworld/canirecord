31f8ef158bea1b432e785042e1f81e42bdafe295
nearworld <artemismars2@outlook.com>
2024-01-31 15:08:35 +0900

    fix: create sub paths signin and singup of auth path

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-[100vh] w-full grid place-items-center">
       <section className="w-2/3 h-5/6 flex flex-col justify-center items-center gap-5">
-        <Link href="auth" className="self-end">
+        <Link href="auth/signup" className="self-end">
           <Button>signup</Button>
         </Link>
         <Card className="w-full h-[70vh]"></Card>
