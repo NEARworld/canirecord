90703dc852b8d4a91d7f12d86fd661ea66729093
nearworld <artemismars2@outlook.com>
2024-01-31 14:54:28 +0900

    feat: put Card and Button on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="min-h-[100vh] w-full grid place-items-center">
+      <section className="w-2/3 h-5/6 flex flex-col justify-center items-center gap-5">
+        <Button className="self-end">signup</Button>
+        <Card className="w-full h-[70vh]"></Card>
+      </section>
+    </main>
+  );
 }
