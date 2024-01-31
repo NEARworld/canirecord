1f1b2ec807b260b1b7539c623d2ce207a2016e55
nearworld <artemismars2@outlook.com>
2024-01-31 15:00:31 +0900

    feat: move to signup page on button click

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="min-h-[100vh] w-full grid place-items-center">
       <section className="w-2/3 h-5/6 flex flex-col justify-center items-center gap-5">
-        <Button className="self-end">signup</Button>
+        <Link href="auth" className="self-end">
+          <Button>signup</Button>
+        </Link>
         <Card className="w-full h-[70vh]"></Card>
       </section>
     </main>
